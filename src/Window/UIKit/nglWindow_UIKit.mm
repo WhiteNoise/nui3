3827eab33aa332b1ad02c1c64bb60ddca3be0cb2
David Wallin <dwallin33@gmail.com>
2011-08-30 08:34:43 -0400

    hack away statusbar

--- src/Window/UIKit/nglWindow_UIKit.mm
+++ src/Window/UIKit/nglWindow_UIKit.mm
@@ -1070,13 +1070,13 @@ void nglWindow::SetState (StateChange State)
 	switch (State)
 	{
 		case eHide:
-			[pApp setStatusBarHidden:FALSE animated:TRUE];
+			[pApp setStatusBarHidden:TRUE animated:TRUE];
 			break;
 		case eShow:
-			[pApp setStatusBarHidden:FALSE animated:TRUE];
+			[pApp setStatusBarHidden:TRUE animated:TRUE];
 			break;
 		case eMinimize:
-			[pApp setStatusBarHidden:FALSE animated:TRUE];
+			[pApp setStatusBarHidden:TRUE animated:TRUE];
 			break;
 		case eMaximize:
 		{
